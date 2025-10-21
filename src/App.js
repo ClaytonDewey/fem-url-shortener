eb32afd0a9548f2ce089885ab006d8f404507934
ClaytonDewey <clay@dryadmedia.com>
2025-10-21 11:59:01 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,122 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className='App'>
+      Features
+      <br />
+      Pricing
+      <br />
+      Resources
+      <br />
+      <br />
+      Login
+      <br />
+      Sign Up
+      <br />
+      <br />
+      More than just shorter links
+      <br />
+      <br />
+      Build your brand&rsquo;s recognition and get detailed insights on how your
+      links are performing.
+      <br />
+      <br />
+      Get Started
+      <br />
+      <br />
+      Shorten a link here...
+      <br />
+      <br />
+      Shorten It!
+      <br />
+      <br />
+      Advanced Statistics
+      <br />
+      <br />
+      Track how your links are performing across the web with our advanced
+      statistics dashboard.
+      <br />
+      <br />
+      Brand Recognition
+      <br />
+      <br />
+      Boost your brand recognition with each click. Generic links don&rsquo;t
+      mean a thing. Branded links help instil confidence in your content.
+      <br />
+      <br />
+      Detailed Records
+      <br />
+      <br />
+      Gain insights into who is clicking your links. Knowing when and where
+      people engage with your content helps inform better decisions.
+      <br />
+      <br />
+      Fully Customizable
+      <br />
+      <br />
+      Improve brand awareness and content discoverability through customizable
+      links, supercharging audience engagement.
+      <br />
+      <br />
+      Boost your links today
+      <br />
+      <br />
+      Get Started
+      <br />
+      <br />
+      <div>
+        Features
+        <br />
+        <br />
+        Link Shortening
+        <br />
+        Branded Links
+        <br />
+        Analytics
+        <br />
+      </div>
+      <div>
+        Resources
+        <br />
+        <br />
+        Blog
+        <br />
+        Developers
+        <br />
+        Support
+        <br />
+      </div>
+      <div>
+        Company
+        <br />
+        <br />
+        About
+        <br />
+        Our Team
+        <br />
+        Careers
+        <br />
+        Contact
+        <br />
+      </div>
+      <footer class='attribution'>
+        Challenge by{' '}
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          href='https://www.frontendmentor.io?ref=challenge'
+          rel='noreferrer'
+          target='_blank'>
+          Frontend Mentor
         </a>
-      </header>
+        . Coded by{' '}
+        <a
+          href='https://www.claytondewey.com/'
+          rel='noreferrer'
+          target='_blank'>
+          Clayton Dewey
+        </a>
+        .
+      </footer>
     </div>
   );
 }
