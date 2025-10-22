9a9c9623c83cc929d00872ee01e885e9223d7cb1
ClaytonDewey <clay@dryadmedia.com>
2025-10-22 14:17:50 -0500

    feat: Add nav toggle function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,27 @@
+import { useState } from 'react';
 import Button from './Button';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className='navbar'>
       <Button
         type='button'
-        className='navbar__toggle'
-        expanded='false'
-        aria-label='Toggle menu'>
+        className={`navbar__toggle ${isOpen ? 'open' : ''}`}
+        expanded={isOpen}
+        aria-label='Toggle menu'
+        onClick={handleClick}>
         <span className='sr-only'>Toggle menu</span>
         <span className='icon-bar top-bar'></span>
         <span className='icon-bar middle-bar'></span>
         <span className='icon-bar bottom-bar'></span>
       </Button>
-      <div className='navbar__wrapper'>
+      <div className={`navbar__wrapper ${isOpen ? 'open' : ''}`}>
         <ul className='links'>
           <li>
             <a href='/'>Features</a>
