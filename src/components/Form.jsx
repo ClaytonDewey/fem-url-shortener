25a924d866e0eddf44a35bb0b2c036a77dfd07bb
ClaytonDewey <clay@dryadmedia.com>
2025-10-23 12:31:15 -0500

    feat: Add basic onSubmit & error handling

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,20 +4,33 @@ import Button from './Button';
 
 const Form = () => {
   const [url, setUrl] = useState('');
+  const [error, setError] = useState('');
 
   const handleChange = (e) => {
     setUrl(e.target.value);
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (url.trim() === '') {
+      setError('Please add a link');
+    } else {
+      // alert(url);
+      setError('');
+    }
+  };
+
   return (
     <section className='form__wrapper'>
       <div className='container'>
         <div className='form'>
-          <form>
+          <form onSubmit={handleSubmit}>
             <Input
               label='Link to shorten'
               placeholder='Shorten a link here...'
               name='url'
               value={url}
+              error={error}
               onChange={handleChange}
             />
             <Button className='btn' type='submit'>
