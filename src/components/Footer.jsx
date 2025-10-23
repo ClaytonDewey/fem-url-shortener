b6546226eaac2e9dba02eaa84c261d8045591adf
Clayton Dewey <clay@dryadmedia.com>
2025-10-22 21:06:33 -0500

    refactor: Add content to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,60 @@
 const Footer = () => {
   return (
-    <footer class='attribution'>
-      Challenge by{' '}
-      <a
-        href='https://www.frontendmentor.io?ref=challenge'
-        rel='noreferrer'
-        target='_blank'>
-        Frontend Mentor
-      </a>
-      . Coded by{' '}
-      <a href='https://www.claytondewey.com/' rel='noreferrer' target='_blank'>
-        Clayton Dewey
-      </a>
-      .
+    <footer className='footer'>
+      <div className='container'>
+        <div>
+          Features
+          <br />
+          <br />
+          Link Shortening
+          <br />
+          Branded Links
+          <br />
+          Analytics
+          <br />
+        </div>
+        <div>
+          Resources
+          <br />
+          <br />
+          Blog
+          <br />
+          Developers
+          <br />
+          Support
+          <br />
+        </div>
+        <div>
+          Company
+          <br />
+          <br />
+          About
+          <br />
+          Our Team
+          <br />
+          Careers
+          <br />
+          Contact
+          <br />
+        </div>
+      </div>
+      <div className='attribution'>
+        Challenge by{' '}
+        <a
+          href='https://www.frontendmentor.io?ref=challenge'
+          rel='noreferrer'
+          target='_blank'>
+          Frontend Mentor
+        </a>
+        . Coded by{' '}
+        <a
+          href='https://www.claytondewey.com/'
+          rel='noreferrer'
+          target='_blank'>
+          Clayton Dewey
+        </a>
+        .
+      </div>
     </footer>
   );
 };
