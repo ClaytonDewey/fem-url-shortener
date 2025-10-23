aab035ed0f17146bc43075a988c6c777d8fc0246
ClaytonDewey <clay@dryadmedia.com>
2025-10-23 10:34:51 -0500

    style: Update CTA layout styles

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -2,10 +2,9 @@ const Cta = () => {
   return (
     <div className='cta'>
       <div className='container'>
-        Boost your links today
-        <br />
-        <br />
-        <a href='/' className='btn btn-cta'>
+        <h2>Boost your links today</h2>
+
+        <a href='/' className='btn btn-cta btn-pill'>
           Get Started
         </a>
       </div>
